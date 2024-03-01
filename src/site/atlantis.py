d67ffa2711afb39f08b4e8e4db8ace14a7ac3021
André Costa <andrecardosocosta@enta.pt>
2024-03-01 22:15:49 -0100

    update

--- src/site/atlantis.py
+++ src/site/atlantis.py
@@ -1,16 +1,13 @@
 import chess
-import numpy as np
-import time
 import random
 
 class Engine:
     def __init__(self, tab, profmax, cor):
         self.tab = tab
         self.cor = cor
-        self.nodes_processed = 0
         self.profmax = profmax
 
-    valorPeaoBranco = np.array([
+    valorPeaoBranco = [
         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
         0.05, 0.1, 0.1, -0.2, -0.2, 0.1, 0.1, 0.05,
         0.05, -0.05, -0.1, 0.0, 0.0, -0.1, -0.05, 0.05,
@@ -19,11 +16,11 @@ class Engine:
         0.1, 0.1, 0.2, 0.3, 0.3, 0.2, 0.1, 0.1,
         0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-    ])
+    ]
 
-    valorPeaoPreto = np.array(list(reversed(valorPeaoBranco)))
+    valorPeaoPreto = list(reversed(valorPeaoBranco))
 
-    valorCavaloBranco = np.array([
+    valorCavaloBranco = [
         -0.5, -0.4, -0.3, -0.3, -0.3, -0.3, -0.4, -0.5,
         -0.4, -0.2, 0.0, 0.0, 0.0, 0.0, -0.2, -0.4,
         -0.3, 0.0, 0.1, 0.15, 0.15, 0.1, 0.0, -0.3,
@@ -32,24 +29,24 @@ class Engine:
         -0.3, 0.05, 0.1, 0.15, 0.15, 0.1, 0.05, -0.3,
         -0.4, -0.2, 0.0, 0.05, 0.05, 0.0, -0.2, -0.4,
         -0.5, -0.4, -0.3, -0.3, -0.3, -0.3, -0.4, -0.5
-    ])
+    ]
 
-    valorCavaloPreto = np.array(list(reversed(valorCavaloBranco)))
+    valorCavaloPreto = list(reversed(valorCavaloBranco))
 
-    valorBispoBranco = np.array([
-    -0.2, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2,
-    -0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, -0.1,
-    -0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, -0.1,
-    -0.1, 0.0, 0.1, 0.1, 0.1, 0.1, 0.0, -0.1,
-    -0.1, 0.05, 0.05, 0.1, 0.1, 0.05, 0.05, -0.1,
-    -0.1, 0.0, 0.05, 0.1, 0.1, 0.05, 0.0, -0.1,
-    -0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1,
-    -0.2, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2
-])
+    valorBispoBranco = [
+        -0.2, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2,
+        -0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, -0.1,
+        -0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, -0.1,
+        -0.1, 0.0, 0.1, 0.1, 0.1, 0.1, 0.0, -0.1,
+        -0.1, 0.05, 0.05, 0.1, 0.1, 0.05, 0.05, -0.1,
+        -0.1, 0.0, 0.05, 0.1, 0.1, 0.05, 0.0, -0.1,
+        -0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1,
+        -0.2, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2
+    ]
 
-    valorBispoPreto = np.array(list(reversed(valorBispoBranco)))
+    valorBispoPreto = list(reversed(valorBispoBranco))
 
-    valorTorreBranca = np.array([
+    valorTorreBranca = [
         0.0, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0, 0.0,
         -0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05,
         -0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05,
@@ -58,11 +55,11 @@ class Engine:
         -0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05,
         0.05, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.05,
         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-    ])
+    ]
 
-    valorTorrePreta = np.array(list(reversed(valorTorreBranca)))
+    valorTorrePreta = list(reversed(valorTorreBranca))
 
-    valorDamaBranca = np.array([
+    valorDamaBranca = [
         -0.2, -0.1, -0.1, -0.05, -0.05, -0.1, -0.1, -0.2,
         -0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1,
         -0.1, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.1,
@@ -71,11 +68,9 @@ class Engine:
         -0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.0, -0.1,
         -0.1, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0, -0.1,
         -0.2, -0.1, -0.1, -0.05, -0.05, -0.1, -0.1, -0.2
-    ])
+    ]
 
-    valorDamaPreta = np.array(list(reversed(valorDamaBranca)))
-
-    valorReiBranco = np.array([
+    valorReiBranco = [
         0.02, 0.03, 0.01, 0.0, 0.0, 0.1, 0.3, 0.2,
         0.2, 0.2, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2,
         -0.1, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.1,
@@ -84,22 +79,10 @@ class Engine:
         -0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3,
         -0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3,
         -0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3
-    ])
+    ]
+    valorReiPreto = list(reversed(valorReiBranco))
 
-    valorReiPreto = np.array(list(reversed(valorReiBranco)))
-
-    valorReiBrancoFinal = np.array([
-        0.5, -0.3, -0.3, -0.3, -0.3, -0.3, -0.3, -0.5,
-        -0.3, -0.3,  0.0,  0.0,  0.0,  0.0, -0.3, -0.3,
-        -0.3, -0.1,  0.2,  0.3,  0.3,  0.2, -0.1, -0.3,
-        -0.3, -0.1,  0.3,  0.4,  0.4,  0.3, -0.1, -0.3,
-        -0.3, -0.1,  0.3,  0.4,  0.4,  0.3, -0.1, -0.3,
-        -0.3, -0.1,  0.2,  0.3,  0.3,  0.2, -0.1, -0.3,
-        -0.3, -0.2, -0.1,  0.0,  0.0, -0.1, -0.2, -0.3,
-        -0.5, -0.4, -0.3, -0.2, -0.2, -0.3, -0.4, -0.5
-    ])
-
-    valorReiPretoFinal = np.array(list(reversed(valorReiBrancoFinal)))
+    valorDamaPreta = list(reversed(valorDamaBranca))
 
     def ordenarJogadas(self):
         def ordenador(jog):
@@ -109,14 +92,14 @@ class Engine:
             return valor  
 	    
         if self.cor == chess.WHITE:
-            in_order = np.array(sorted(self.tab.legal_moves, key=ordenador, reverse=self.tab.turn==chess.BLACK))
+            in_order = sorted(self.tab.legal_moves, key=ordenador, reverse=self.tab.turn==chess.BLACK)
         elif self.cor == chess.BLACK:
-            in_order = np.array(sorted(self.tab.legal_moves, key=ordenador, reverse=self.tab.turn==chess.WHITE))
+            in_order = sorted(self.tab.legal_moves, key=ordenador, reverse=self.tab.turn==chess.WHITE)
 
-        if self.tab.fullmove_number < 4:
-            filter = np.array(in_order[-10:])
+        if self.tab.fullmove_number < 10:
+            filter = in_order[-7:]
         else:
-            filter = np.array(in_order[-12:])
+            filter = in_order[-10:]
 
         return list(filter)
 
@@ -130,46 +113,26 @@ class Engine:
             valorPeca = 1
         
         elif self.tab.piece_type_at(quad) == chess.KNIGHT:
-            valorPeca = 3.3
+            valorPeca = 3.2
 
         elif self.tab.piece_type_at(quad) == chess.BISHOP:
-            valorPeca = 3.4
+            valorPeca = 3.33
         
         elif self.tab.piece_type_at(quad) == chess.ROOK:
-            valorPeca = 5
+            valorPeca = 5.1
         
         elif self.tab.piece_type_at(quad) == chess.QUEEN:
-            valorPeca = 9
+            valorPeca = 8.8
         
-        if self.tab.color_at(quad) == chess.BLACK:
-            return valorPeca
-        
-        else:
+        if self.tab.color_at(quad) != self.cor:
             return -valorPeca
-
-    def verificarFinal(self):
-        damas = 0
-        pecasmen = 0
-        for i in chess.SQUARES:
-            if self.tab.piece_type_at(i) == chess.BISHOP:
-                pecasmen += 1
-            
-            elif self.tab.piece_type_at(i) == chess.KNIGHT:
-                pecasmen += 1
-            
-            elif self.tab.piece_type_at(i) == chess.QUEEN:
-                damas += 1
-        
-        if damas == 0 and pecasmen <= 2:
-            return True
         
         else:
-            return False
+            return valorPeca
 
     def mapeamento(self):
         valor = 0
-        final = self.verificarFinal()
-
+        
         for i in chess.SQUARES:
             if self.tab.color_at(i) == chess.WHITE:    
                 if self.tab.piece_type_at(i) == chess.PAWN:
@@ -182,11 +145,9 @@ class Engine:
                     valor += self.valorTorreBranca[i]
                 elif self.tab.piece_type_at(i) == chess.QUEEN:
                     valor += self.valorDamaBranca[i]
-                elif self.tab.piece_type_at(i) == chess.KING and final == False:
+                elif self.tab.piece_type_at(i) == chess.KING:
                     valor += self.valorReiBranco[i]
-
-                elif self.tab.piece_type_at(i) == chess.KING and final == True:
-                    valor += self.valorReiBrancoFinal[i]
+            
             else:
                 if self.tab.piece_type_at(i) == chess.PAWN:
                     valor += self.valorPeaoPreto[i]
@@ -198,20 +159,21 @@ class Engine:
                     valor += self.valorTorrePreta[i]
                 elif self.tab.piece_type_at(i) == chess.QUEEN:
                     valor += self.valorDamaPreta[i]
-                elif self.tab.piece_type_at(i) == chess.KING and final == False:
+                elif self.tab.piece_type_at(i) == chess.KING:
                     valor += self.valorReiPreto[i]
-                
-                elif self.tab.piece_type_at(i) == chess.KING and final == True:
-                    valor += self.valorReiPretoFinal[i]
 
         return valor
     
     def abertura(self):
-        if self.tab.fullmove_number <= 4:
-            ab = 1/30 * self.tab.legal_moves.count()
+        if self.tab.fullmove_number <= 10:
+            if self.tab.turn == chess.WHITE:
+                ab = 1/30 * self.tab.legal_moves.count()
+            else:
+                ab = 1/30 * self.tab.legal_moves.count()
         else:
             ab = 0
-
+	
+    
         return ab
     
     def avalTab(self):
@@ -219,7 +181,7 @@ class Engine:
 
         for i in range(64):
             valor += self.valorPecas(chess.SQUARES[i])
-        valor += self.mate() + self.abertura() + self.mapeamento() + 0.001 * random.random()
+        valor += self.mate() + self.abertura() + self.mapeamento() + 0.01 * random.random()
         
         return valor
     
@@ -234,7 +196,6 @@ class Engine:
            
     def minimax(self, cand, prof, alpha=float("-inf"), beta=float("inf")):
         if (prof == self.profmax or self.tab.legal_moves.count() == 0):
-            self.nodes_processed += 1
             return self.avalTab()
     
         else:
@@ -287,18 +248,10 @@ class Engine:
                     return novoCand
                 else:
                     return jog
-    
+            
 def generateMove(fen, profmax, cor):
     tab = chess.Board(fen)
     engine = Engine(tab, profmax, cor)
-    start_time = time.time()
     jog = engine.melhorJogada()
-    end_time = time.time()
-
-    duracao = end_time - start_time
-
-    nodes_per_second = engine.nodes_processed / duracao
-
-    print(f"Nodes per second: {nodes_per_second:.2f}")
-
+    print(engine.abertura())
     return jog
